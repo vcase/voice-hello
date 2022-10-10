44dfe912b06c2b3513c09ce570da1d9b55751c1a
vcase <vincent.case@gmail.com>
2022-10-09 20:32:14 -0400

    Support swagger-ui and pprof endpoints

--- hello_impl.go
+++ hello_impl.go
@@ -4,14 +4,14 @@ import (
 	"context"
 	"fmt"
 
-	helloserver "github.com/vcase/voice-hello/generated/hello"
+	helloserver "github.com/vcase/voice-hello/generated/grpc/hello"
 )
 
 type helloServer struct {
 	helloserver.UnimplementedHelloServer
 }
 
-func (server *helloServer) Hello(ctx context.Context, request *helloserver.HelloRequest) (*helloserver.HelloResponse, error) {
+func (server *helloServer) Greet(ctx context.Context, request *helloserver.HelloRequest) (*helloserver.HelloResponse, error) {
 	greeting := request.Greeting
 	if greeting == "" {
 		greeting = "hello"
